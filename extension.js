3cc9c62fd0fba9ee6195c0d4715610a16e6f1d75
cptyuan <cptyuan@gmail.com>
2026-05-28 18:27:55 +0800

    Update extension.js

--- extension.js
+++ extension.js
@@ -1,9 +1,9 @@
-import OBR from "https://esm.sh/@owlbear-rodeo/sdk";
+const OBR = window.OBR;
 
 OBR.onReady(() => {
     OBR.tool.createAction({
         id: "com.cptyuan.token-labels.action",
-        icon: "tag",
+        icons: ["https://raw.githubusercontent.com/google/material-design-icons/master/src/action/label/svg/production/ic_label_24px.svg"],
         tooltips: "Token 标注",
         onClick: () => {
             OBR.notification.show("按钮已点击！", "info");

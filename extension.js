07f4e85d711a779190da5a4f358e0900d947f5b6
cptyuan <cptyuan@gmail.com>
2026-05-28 18:09:15 +0800

    Update extension.js

--- extension.js
+++ extension.js
@@ -3,7 +3,7 @@ const OBR = window.OBR;
 OBR.onReady(() => {
     OBR.tool.createAction({
         id: "com.cptyuan.token-labels.action",
-        icons:  ["/tag.svg"],
+        icon: "tag",
         tooltips: "Token 标注",
         onClick: () => {
             OBR.notification.show("按钮已点击！", "info");
